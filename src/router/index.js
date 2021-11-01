7771b275ecd01c9a82928bcc1c1d17957403dccb
kumankoishi.py <7iggif@naver.com>
2021-11-02 08:09:23 +0900

    Establishing basic structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import WriteMemo from '../views/WriteMemo.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Write A Memo',
+    component: WriteMemo
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/memolist',
+    name: 'Memo List',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/MemoList.vue')
   }
 ]
 
