a98e7f5ee93c55fb1003eadb62b319a42b3471df
kumankoishi.py <7iggif@naver.com>
2021-11-03 06:29:30 +0900

    Added Vuex states and functions

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,39 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const initMemos = localStorage.getItem('memos');
+
 export default new Vuex.Store({
   state: {
+    memos: (initMemos ? JSON.parse(initMemos) : [])
   },
   mutations: {
+    addMemo (state, memoContent) {
+      state.memos.push({
+        id: `${Date.now()}`,
+        content: memoContent
+      });
+    },
+    removeMemo (state, memoId) {
+      const newMemoList = state.memos.filter((item) => (item.id !== memoId));
+      state.memos = newMemoList;
+    },
+    modifyMemo (state, memoId, newMemoContent) {
+      let memoIndex = 0;
+      while (memoIndex < state.memos.length) {
+        if (state.memos[memoIndex].id === memoId) {
+          break;
+        }
+        memoIndex++;
+      }
+      state.memos[memoIndex].content = newMemoContent;
+    }
   },
   actions: {
+    saveMemosToStorage (state) {
+      const content = JSON.stringify(state.memos);
+      localStorage.setItem('memos', content);
+    }
   },
   modules: {
   }
