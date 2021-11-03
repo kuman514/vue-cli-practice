2f18b19a81752a192f1ca4413fc8fae6646baefc
kumankoishi.py <7iggif@naver.com>
2021-11-04 08:34:50 +0900

    MemoList shows MemoListItems

--- src/views/MemoList.vue
+++ src/views/MemoList.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="memolist">
+    <h1>Written Memos</h1>
+    <MemoListItems/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import MemoListItems from '@/components/MemoListItems.vue'
+
+export default {
+  name: 'MemoList',
+  components: {
+    MemoListItems
+  }
+}
+</script>
