f3c8cedb9cad57883e045e574fae3069301b2892
kumankoishi.py <7iggif@naver.com>
2021-11-04 08:34:30 +0900

    Created MemoListItems

---
+++ src/components/MemoListItems.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="memolistitems">
+    <li v-for="memoItem in memoItems" v-bind:key="memoItem.id">
+      <MemoItem :memoContnet="memoItem.content"/>
+    </li>
+  </ul>
+</template>
+
+<script>
+import store from '../store/index';
+import MemoItem from './MemoItem.vue';
+
+export default {
+  name: 'MemoListItems',
+  data() {
+    return {
+      memoItems: store.getters.memos
+    };
+  },
+  components: {
+    MemoItem
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+}
+a {
+  color: #42b983;
+}
+</style>
